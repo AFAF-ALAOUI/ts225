1f83e2869cdebc0c7593bce1f1b649d1896ac766
AFAF-ALAOUI <62150003+AFAF-ALAOUI@users.noreply.github.com>
2020-12-12 22:48:32 +0100

    Add files via upload

--- barre.m
+++ barre.m
@@ -3,8 +3,7 @@ close all
 
 addpath 'Data' ;
 
-N=95; %unités
-
+N=95; %unité
 I = double(imread('ean13.jpg'));
 [h,w,z] = size(I);
 
@@ -21,64 +20,65 @@ end
 
 %% Lancer aléatoire d'un rayon
 [A,B] = ginput(2);
-C = A;
-A = [A(1),B(1)];
-B = [C(2),B(2)];
 
 line(A,B)
-distAB = sqrt((B(2)-A(2))^2+(B(1)-A(1))^2);
+distAB = sqrt((A(2)-A(1))^2+(B(2)-B(1))^2);
 Npts = floor(distAB*2);
 samples = zeros(2,Npts);
-for i=1:Npts
-    samples(:,i) = A+i/Npts*(B-A);
-    
-end
-
-u=1;
-v=1;
-for i=1:length(samples(1,:))
-    im(u,v) = imgY(round(samples(1,i)),round(samples(2,i)));
-    v= v+1;
-end
-
-figure,plot(im);
-[min1,argmin1] = min(im(1:floor(length(im)/3)));
-[min2,argmin2] = min(im(floor(2*length(im)/3) : length(im)));
-
-
-%Début et fin effectifs du code barre
-A_eff = samples(:,argmin1);
-B_eff = samples(:,floor((length(im))/2) +argmin2);
-
-% Nouveau Echantillonnage
-
-distAB_eff = sqrt((B_eff(2)-A_eff(2))^2+(B_eff(1)-A_eff(1))^2);
-
-Npts = N;
-samples_eff = zeros(2,Npts);
-for i=1:Npts
-    samples_eff(:,i) = A_eff+i/Npts*(B_eff-A_eff);
+for i=0:Npts-1
+    samples(:,i+1) = [A(1);B(1)]+i/(Npts-1)*([A(2);B(2)]-[A(1);B(1)]);
     
 end
 
 %interpolation
-u=1;
 v=1;
-for i=1:length(samples_eff(1,:))
-    im_eff(u,v) = imgY(round(samples_eff(1,i)),round(samples_eff(2,i)));
+im = zeros(1,length(samples(1,:)));
+for i=1:length(samples(1,:))
+    im(1,v) = imgY(round(samples(2,i)),round(samples(1,i)));
     v= v+1;
+end
+figure,plot(im);
+figure,
+H = histogram(im,256);
+
+%Otsu
+crit = critere( H.Values, 256);
+value = (H.BinEdges(crit)+H.BinEdges(crit+1))/2;
+binarisation = im  > value;
+
+%Début et fin effectifs du code barre
+for i=1:length(binarisation)
+    if(binarisation(i) == 0)
+        A_eff = samples(:,i);
+        break;
+    end
+end
+i = length(binarisation);
+while (i~=0)
+    if(binarisation(i) == 0)
+        B_eff = samples(:,i);
+        break;
+    end
+    i = i-1;
 end
 
 
-figure,
-H = histogram(im_eff,256);
+% Nouveau Echantillonnage
 
-crit = critere( H.Values, 256);
-value = (H.BinEdges(crit)+H.BinEdges(crit+1))/2;
+distAB_eff = sqrt((B_eff(2)-A_eff(2))^2+(B_eff(1)-A_eff(1))^2);
+unit_base = floor(distAB_eff/95);
+Npts = unit_base*N;
+samples_eff = zeros(2,Npts);
 
-binarisation = im_eff  > value;
-%plot(binarisation);
-
-
-
+for i=0:Npts-1
+    samples_eff(:,i+1) = A_eff+i/(Npts-1)*(B_eff-A_eff);  
+end
+%interpolation
+v=1;
+im_eff = zeros(1,length(samples_eff(1,:)));
+for i=1:length(samples_eff(1,:))
+    im_eff(1,v) = imgY(round(samples_eff(2,i)),round(samples_eff(1,i)));
+    v= v+1;
+end
+binarisation_eff = im_eff  > value;
 
