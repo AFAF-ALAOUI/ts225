073c0679b5011dbd9b59fa0958308227864a9aef
AFAF-ALAOUI <62150003+AFAF-ALAOUI@users.noreply.github.com>
2020-12-16 21:31:20 +0100

    Delete coherence.m

--- coherence.m
+++
@@ -1,3 +0,0 @@
-function D_xy = coherence(w,i,j,Ix,Iy)
-    D_xy= sqrt((Tenseur(w,i,j,Ix,Ix)-Tenseur(w,i,j,Iy,Iy))^2 + 4*Tenseur(w,i,j,Ix,Iy)^2)/(Tenseur(w,i,j,Ix,Ix)+Tenseur(w,i,j,Iy,Iy));    
-end
