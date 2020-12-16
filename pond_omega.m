e2baccfa79f4c61ce7c2a5e372fd0e4fb77b7fb9
AFAF-ALAOUI <62150003+AFAF-ALAOUI@users.noreply.github.com>
2020-12-16 21:25:49 +0100

    Add files via upload

--- pond_omega.m
+++ pond_omega.m
@@ -1,5 +1,5 @@
 function H = pond_omega(sigmaT)
-    P = -3*round(sigmaT)-1:3*round(sigmaT)+1;    
+    P = floor(-3*sigmaT):floor(3*sigmaT)+1;    
     [X,Y] = meshgrid(P);
-    H = 1/(2*pi*sigmaT^2).*exp(-(X.^2+Y.^2)/(2*sigmaT^2));
+    H = exp(-(X.^2+Y.^2)/(2*sigmaT^2))/(2*pi*sigmaT^2);
 end
