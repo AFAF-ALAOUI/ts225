6b2c4204a256cb4079808c8952c69994ed7ba187
AFAF-ALAOUI <62150003+AFAF-ALAOUI@users.noreply.github.com>
2021-01-17 17:26:59 +0100

    Add files via upload

--- final.m
+++ final.m
@@ -46,18 +46,38 @@ figure,imshow(D_bin)
 L = bwlabel(D_bin);
 Xm=zeros(max(L(:)),1); 
 Ym=zeros(max(L(:)),1);
-for i=2:2%max(L(:))
-    [x,y]=find(R==i);
+for i=1:max(L(:))
+    [x,y]=find(L==i);
     Xm(i,1)=mean(x);
     Ym(i,1)=mean(y);
     C= 1/length(x)*[sum((x-Xm(i,1)).^2) sum((x-Xm(i,1)).*(y-Ym(i,1))); sum((x-Xm(i,1)).*(y-Ym(i,1))) sum((y-Ym(i,1)).^2)];
     [V,D]=eig(C);
     [vp,ivp]=max(max(D));
+    [vp2,ivp2]=min(min(D));
     V2=V(:,ivp);
-    project = projection(x-Xm(i,1),y-Ym(i,1),V2);
-    minimum = min(project);
-    maximum = max(project);
-    distance= maximum-minimum;
+    V1=V(:,ivp2);
+    project = projection(x,y,V2);
+    project2 = projection(x,y,V1);
+    longueur= max(project)-min(project);
+    largeur = max(project2)-min(project2);
+    
+    if (longueur  >=95 && largeur >=longueur/2 )
+        correct =0;
+        n_lancer =0;
+        while(correct ~=1 && 100 > n_lancer )
+            angle = randi([-80,80]);
+            n_lancer = n_lancer+1;
+            x1 = min(-longueur*tand(angle)/2-1 + Ym(i,1),h);
+            y1 = min(-longueur/2-1 +Xm(i,1),w);
+        
+            x2 = min(longueur*tand(angle)/2-1 + Ym(i,1),h);
+            y2 = min(longueur/2 +Xm(i,1)-1,w);
+            line([x1,x2],[y1,y2]);
+        
+            [correct,chiffre_tot] = lancer_aleatoire(imgY,N,[y1,y2],[x1,x2]);
+        end
+        
+    end
     
 end
 
---
+++ lancer_aleatoire.m
@@ -0,0 +1,77 @@
+function [correct,chiffre_tot] = lancer_aleatoire(imgY,N,A,B)
+    correct =0;
+    distAB = sqrt((A(2)-A(1))^2+(B(2)-B(1))^2);
+    Npts = floor(distAB*5);
+    samples = zeros(2,Npts);
+    for i=0:Npts-1
+        samples(:,i+1) = [A(1);B(1)]+i/(Npts-1)*([A(2);B(2)]-[A(1);B(1)]);
+    end
+
+    %interpolation
+    v=1;
+    im = zeros(1,length(samples(1,:)));
+    for i=1:length(samples(1,:))
+        im(1,v) = imgY(round(samples(2,i)),round(samples(1,i)));
+        v= v+1;
+    end
+    figure,plot(im);
+    figure,
+    H = histogram(im,256);
+
+    %% Otsu
+    [crit,indice] = critere( H.Values, 256);
+    figure,plot(crit),
+    xlim([40,250]),title("Critère d'Otsu")
+    value = (H.BinEdges(indice)+H.BinEdges(indice+1))/2;
+    binarisation = im  > value;
+
+    %Début et fin effectifs du code barre
+    for i=1:length(binarisation)
+        if(binarisation(i) == 0)
+            A_eff = samples(:,i);
+            break;
+        end
+    end
+    i = length(binarisation);
+    while (i~=1)
+        if(binarisation(i) == 0)
+            B_eff = samples(:,i);
+            break;
+        end
+        i = i-1;
+    end
+
+
+    % Nouveau Echantillonnage
+    distAB_eff = sqrt((B_eff(2)-A_eff(2))^2+(B_eff(1)-A_eff(1))^2);
+    unit_base = floor(distAB_eff/95);
+    Npts = unit_base*N;
+    samples_eff = zeros(2,Npts);
+
+    for i=0:Npts-1
+        samples_eff(:,i+1) = A_eff+i/(Npts-1)*(B_eff-A_eff);  
+    end
+    %interpolation
+    v=1;
+    im_eff = zeros(1,length(samples_eff(1,:)));
+    for i=1:length(samples_eff(1,:))
+        im_eff(1,v) = imgY(round(samples_eff(2,i)),round(samples_eff(1,i)));
+        v= v+1;
+    end
+    binarisation_eff = im_eff  > value;
+
+    %%
+     I_code=[binarisation_eff(3*unit_base+1:45*unit_base) binarisation_eff(50*unit_base+1:end-3*unit_base)];
+     [chiffres,famille,ressemblance] = chiffrement(I_code,unit_base);
+     chiffre_tot = [chiffre_famille(famille(1:6)) chiffres];
+     
+      %% Clé de contrôle
+    key=0;
+    for i=1:6
+        key = key + chiffre_tot(2*i-1)+3*chiffre_tot(2*i);
+    end
+
+    if(10-mod(key,10)==chiffre_tot(13))
+        correct = 1;
+    end
+end
